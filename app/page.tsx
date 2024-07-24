5b11708581dbdb29ebc320923e8c6dd7e430b1c7
YakhyaevGadji <gadji180105@gmail.com>
2024-07-24 18:58:53 +0300

    add: button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-      <h1>home</h1>
+      <>
+        <h1>home</h1>
+          <Button variant="outline">teet</Button>
+      </>
   );
 }
